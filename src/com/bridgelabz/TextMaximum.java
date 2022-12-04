440a0c153cbbb6f85a5f5caf27408f559e5f7418
Prince <princek2530@gmail.com>
2022-12-04 12:39:35 +0530

    Added: Welcome Message

---
+++ src/com/bridgelabz/TextMaximum.java
@@ -0,0 +1,7 @@
+package com.bridgelabz;
+
+public class TextMaximum {
+    public static void main(String[] args) {
+        System.out.println("Welcome to find the maximum problem using generics");
+    }
+}
