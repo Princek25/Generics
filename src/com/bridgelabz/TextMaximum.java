acbffe894dd7525d8949e72c92ccc05a2c873d8a
Prince <princek2530@gmail.com>
2022-12-04 19:17:38 +0530

    Added: Given 3 Floats find the maximum

--- src/com/bridgelabz/TextMaximum.java
+++ src/com/bridgelabz/TextMaximum.java
@@ -12,11 +12,22 @@ public class TextMaximum {
         }
         return maximum;
     }
+    public Float checkMaximumFloat(Float num1, Float num2, Float num3) {
+        Float max = num1;
+        if (num2.compareTo(max) > 0)
+            max = num2;
+
+        if (num3.compareTo(max) > 0)
+            max = num3;
+        return max;
+    }
 
     public static void main(String[] args) {
         System.out.println("Welcome to find the maximum problem using generics");
         TextMaximum obj = new TextMaximum();
         System.out.println("The maximum of the three Integer is:- ");
         System.out.println(obj.checkMaximumInteger(10,20,30));
+        System.out.println("The maximum of the three Float is:- ");
+        System.out.println(obj.checkMaximumFloat(10.2f, 20.54f, 30.6f));
     }
 }
